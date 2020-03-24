276327b5ea8774487cc8907892496f4ffc2da04e
BallZ <37939288+JasonDrogo@users.noreply.github.com>
2020-03-24 23:30:41 +0530

    Update getmatchdata.service.ts

--- client/src/app/modules/private/services/getmatchdata.service.ts
+++ client/src/app/modules/private/services/getmatchdata.service.ts
@@ -9,12 +9,12 @@ export class GetmatchdataService {
   constructor(private _http: HttpClient) {}
   public getNewMatchdata(): Observable<any> {
     return this._http.get<any>(
-      "https://cricapi.com/api/matches?apikey=ExyXddeiR5XiBjwe71RQeqDD6nr2"
+      "https://cricapi.com/api/matches?apikey=[Your_API_Key]"
     );
   }
   public getOldMatchdata(): Observable<any> {
     return this._http.get<any>(
-      "https://cricapi.com/api/cricket?apikey=ExyXddeiR5XiBjwe71RQeqDD6nr2"
+      "https://cricapi.com/api/cricket?apikey=[Your_API_Key]"
     );
   }
 }
